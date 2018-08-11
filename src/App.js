c4de9dd181352dbf5eca52817ecdcf67a2d52695
Michael McDevitt <mmcdevi1@gmail.com>
2018-08-11 19:28:17 -0400

    style homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <div className="logo">
+            <span className="bold">M / W / M</span>
+          </div>
+          <div className="menu">
+            <span></span>
+            <span className="short"></span>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="content">
+          <div className="left">
+            <span className="hello">Hello -</span>
+            <h1>
+              I'm <strong>Michael W McDevitt</strong> a passionate <strong>Software Engineer</strong> & an eye for <strong>Minimalistic Design</strong>.
+            </h1>
+            <h1>I'm based in <strong>NYC</strong>, currently working <strong>Nowhere</strong>. But, I'd love to work with <strong>You</strong>. So, let's <strong>Connect</strong>.</h1>
+          </div>
+        </div>
+        <footer>
+          <div className="footer-links">
+            <ul>
+              <li><a className="github" href="https://www.github.com/mmcdevitt" target="_blank">github</a></li>
+              <li><a className="linkedin" href="https://www.linkedin.com/in/michaelwmcdevitt" target="_blank">linkedin</a></li>
+              <li><a className="linkedin" href="mailto:michael.w.mcdevitt@gmail.com">email</a></li>
+            </ul>
+          </div>
+        </footer>
       </div>
     );
   }
