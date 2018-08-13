e2ec981743f04fda885c042c0d54cd31d123f5cd
Michael McDevitt <mmcdevi1@gmail.com>
2018-08-12 21:59:35 -0400

    edit home page content

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
           <span className="hello">Hello -</span>
           <h1>
             I'm <strong>Michael W McDevitt</strong> a passionate <strong>Software Engineer</strong> & an eye for <strong>Minimalistic Design</strong>.
-            While working as an Associate for <strong>Thelius Capital Partners</strong>, I was given an opportunity to develop internal software and fell in love with it.
+            While working at <strong>Thelius Capital Partners</strong>, I was given an opportunity to develop internal software and fell in love with it.
             I code in <strong>Ruby, Ruby on Rails, JavaScript, React and Node</strong>.
           </h1>
           <h1>I'm based in <strong>NYC</strong>, currently working <strong>Nowhere</strong>. But, I'd love to work with <strong>You</strong>. So, let's <strong>Connect</strong>.</h1>
